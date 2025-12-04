829f9c1e8347c9fc8dc497740c5c73b14dfe68b1
AdrienTorchio <adrien.torchio@edu.esiee.fr>
2025-12-04 10:01:18 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #### Imports et définition des variables globales
-
+"""
+Module contenant deux fonctions d'encodage RLE (itérative et récursive).
+"""
 # Mandatory for the recursive solution to work on large inputs
 import sys
 sys.setrecursionlimit(2000)
@@ -9,7 +11,8 @@ sys.setrecursionlimit(2000)
 
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples encodant une chaîne de caractères passée en argument 
+        selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -17,14 +20,23 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
-
-    return [ ]
+    char=[s[0]]
+    count=[1]
+    k=1
+    n=len(s)
+    while k<n:
+        if s[k]==s[k-1]:
+            count[-1]+=1
+        else:
+            char.append(s[k])
+            count.append(1)
+        k+=1
+    return list(zip(char,count))
 
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """retourne la liste de tuples encodant une chaîne de caractères passée en argument 
+        selon un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -32,20 +44,26 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
     # votre code ici
-
+    n=len(s)
     # cas de base
+    if n==0:
+        return []
+    if n==1:
+        return [(s[0],1)]
     # recherche nombre de caractères identiques au premier
+    k=0
+    while k<n and s[k]==s[0]:
+        k+=1
+    block = (s[0],k)
     # appel récursif
-
-    return []
-    
-
+    reste = artcode_r(s[k:])
+    return [block]+reste
 #### Fonction principale
 
 
 def main():
+    """Fonction principale exécutant quelques tests simples."""
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
